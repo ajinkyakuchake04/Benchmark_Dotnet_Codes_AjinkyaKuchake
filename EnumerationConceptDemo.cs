eb16aaf1319121c1ecb7dd280d3add42dd152945
ajinkyakuchake04 <89235977+ajinkyakuchake04@users.noreply.github.com>
2021-11-24 23:10:49 +0530

    Add files via upload

---
+++ EnumerationConceptDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerationDemo
+{
+    internal class Program
+    {
+        enum Days {Sun=100,Mon,Tue,Wed,Thu,Fri,Sat };
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.PrintDay(Days.Sat);
+            /*int startDate = (int)(Days.Mon);
+            int lastDate  = (int)(Days.Sat);
+
+            Console.WriteLine("First Day: {0}",startDate);
+            Console.WriteLine("Last Day: {0}",lastDate);
+            */
+            Console.ReadKey();
+        }
+
+        void PrintDay(Days d)
+        {
+            Console.WriteLine(d);
+        }
+    }
+}
